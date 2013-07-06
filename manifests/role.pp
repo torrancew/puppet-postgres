c2e51e1de28fdef38ec17002ca0ecd28535ead0a
Tray Torrance <devwork@warrentorrance.com>
2013-07-05 20:38:26 -0700

    Add a postgres::role defined type

---
+++ manifests/role.pp
@@ -0,0 +1,50 @@
+# == Define: postgres::role
+#
+# A defined type for creating PostgreSQL roles
+#
+# === Parameters:
+#
+# None
+#
+# === Variables:
+#
+# None
+#
+# === Examples:
+#
+# None
+#
+# === Authors:
+#
+# * Tray Torrance
+#
+# === Copyright:
+#
+# Copyright 2013, Tray Torrance
+# unless otherwise noted.
+#
+define postgres::role(
+  $ensure = 'present',
+) {
+  Class['postgres::service'] -> Postgres::Role[$title]
+
+  $test_user = "psql -c 'SELECT rolname FROM pg_roles' | grep -qE '^$title$'"
+
+  if $ensure == 'present' {
+    exec {
+      "create postgres role $title":
+        command => "createuser $title",
+        unless  => $test_user,
+        user    => $postgres::configure::user;
+    }
+  }
+  else {
+    exec {
+      "drop postgres role $title":
+        command => "createuser $title",
+        onlyif  => $test_user,
+        user    => $postgres::configure::user;
+    }
+  }
+}
+
